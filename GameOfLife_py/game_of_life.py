85b539101cf10a09aa2c728e5337a466cff59526
MrElyazid <elyazid.assouli@gmail.com>
2024-03-24 03:30:21 +0000

    finished, TODO : optimize for memory, the current version is memory expensive

--- GameOfLife_py/game_of_life.py
+++ GameOfLife_py/game_of_life.py
@@ -1,6 +1,5 @@
-
 import pygame
-
+import copy
 
 COLORS = {'WHITE': (255, 255, 255),
             'BLACK': (26, 18, 11)}
@@ -27,42 +26,45 @@ class cell:
     def update_state(self, grid):
 
         live_neighbors_count = 0
-        
+
+        future_state = 0
+         
         neighbors = self.get_neighbors(grid)
 
         for neighbor in neighbors:
-            if neighbor.state == COLORS["WHITE"]:
+            if neighbor.state == COLORS["BLACK"]:
                 live_neighbors_count += 1
            
-
-
-        # update the state of the cell according to the rules in the game 
-
+        # update the state of the cell according to the rules in the game https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
         
         if (live_neighbors_count < 2 and self.state == COLORS['BLACK']): # Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-            self.state = COLORS['WHITE']
-        elif (live_neighbors_count == 2 or live_neighbors_count == 3 and self.state == COLORS['BLACK']): # Any live cell with two or three live neighbors lives on to the next generation.
-            self.state = COLORS['BLACK']
+            future_state = COLORS['WHITE']
+        elif ((live_neighbors_count == 2 or live_neighbors_count == 3) and self.state == COLORS['BLACK']): # Any live cell with two or three live neighbors lives on to the next generation.
+            future_state = COLORS['BLACK']
         elif(live_neighbors_count > 3 and self.state == COLORS['BLACK']): # Any live cell with more than three live neighbors dies, as if by overpopulation.
-            self.state == COLORS["BLACK"]
+            future_state = COLORS["WHITE"]
         elif(live_neighbors_count == 3 and self.state == COLORS['WHITE']): # Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-            self.state == COLORS["BLACK"]
+            future_state = COLORS["BLACK"]
     
+        return future_state if future_state != 0 else self.state
+
+
 
     def get_neighbors(self, grid):
         neighbors = [] 
         r = self.row
         c = self.col
-        for i in range(r-1, r+1):
-            for j in range(c-1, c+1):
-                if (i != r and j != c and in_grid(i,j)):
+        for i in range(r-1, r+2):  
+            for j in range(c-1, c+2):
+                if ((i != r or j != c) and in_grid(i, j)):  # Exclude self
                     neighbors.append(grid[i][j])
         return neighbors
 
 
 
 def in_grid(row, col):
-    return (row <= ROWS and col <= COLS)
+    return (0 <= row < ROWS and 0 <= col < COLS)
+
 
 def make_grid(rows, cols):
     grid = []
@@ -88,8 +90,10 @@ def draw_grid(win, grid):
             pygame.draw.rect(win, grid[i][j].state, rect)
             pygame.draw.rect(win, COLORS['BLACK'], rect, 1)  # Drawing grid lines
 
-            
+
+
 def get_clicked_pos(pos, rows, width):
+
     gap = width // rows
     y,x = pos
 
@@ -99,21 +103,24 @@ def get_clicked_pos(pos, rows, width):
     return row, col
 
 
-
 def main(win, width):
 
-    
     grid = make_grid(ROWS, COLS)
+
+    animation_running = False
 
 
     def start_animation(grid):
+        grid2 = copy.deepcopy(grid)
         for i in range(ROWS):
             for j in range(COLS):
-                grid[i][j].update_state(grid)
-
+                state = grid2[i][j].update_state(grid2)
+                grid[i][j].state = state
+        
 
     run = True
     while run:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -125,9 +132,14 @@ def main(win, width):
                 cell.state = COLORS['BLACK']  # Change cell state to "alive"
 
 
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_SPACE]:  # Check if the space bar is pressed
-                start_animation(grid)
+            if event.type == pygame.KEYDOWN:     # Check for key down event
+                if event.key == pygame.K_SPACE:  # Specifically check if the space bar is pressed
+                    animation_running = not animation_running
+
+
+        if animation_running:
+            start_animation(grid)
+            pygame.time.wait(50)
 
 
         win.fill(COLORS['WHITE'])
